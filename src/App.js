e534adab662fd0fc4bf3d801642d39642a09fc3c
PhilipDeFraties <philipjfry338@gmail.com>
2022-11-06 16:21:09 -0700

    Adds form to App.js for login
    Adds function component useInput.jsx that is custom hook to update state 
    with form input value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import React from 'react';
+import useInput from './components/form/useInput'
 
 function App() {
+  const [usernameProps, resetUsername] = useInput("");
+  const [passwordProps, resetPassword] = useInput("")
+
+  const submit = (e) => {
+    e.preventDefault();
+    alert(`${usernameProps.value}, ${passwordProps.value}`)
+    resetUsername();
+    resetPassword();
+  }
   return (
     <div className="App">
       <h1>Hello Cats</h1>
+      <form onSubmit={submit}>
+        <input
+          {...usernameProps}
+          type="text"
+          placeholder="User Name"
+        />
+        <input
+          {...passwordProps}
+          type="text"
+          placeholder="Password"
+        />
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
