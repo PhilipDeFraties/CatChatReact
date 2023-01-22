55a8128cec1a5c2494449e9c2444a49e1c5d56fa
PhilipDeFraties <philipjfry338@gmail.com>
2023-01-22 15:26:25 -0700

    fixes typo
    creates handleSubmit const
    
    fills out return value with login form, success message, placeholder 
    hyperlink for sign up

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
   const [user, setUser] = useState('');
   const [pwd, setPwd] = useState('');
   const [errMsg, setErrMsg] = useState('');
-  const [succes, setSuccess] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     userRef.current.focus();
@@ -17,11 +17,60 @@ const Login = () => {
     setErrMsg('');
   }, [user, pwd])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user, pwd)
+    setUser('');
+    setPwd('');
+    setSuccess(true);
+  }
 
   return (
-    <div>
- 
-    </div>
+    <>
+      {success ? (
+        <section>
+            <h1>You are logged in!</h1>
+            <br />
+            <p>
+                <a href="#">Go to Home</a>
+            </p>
+        </section>
+      ) : (
+          <section>
+              <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+              <h1>Sign In</h1>
+              <form onSubmit={handleSubmit}>
+                  <label htmlFor="username">Username:</label>
+                  <input
+                      type="text"
+                      id="username"
+                      ref={userRef}
+                      autoComplete="off"
+                      onChange={(e) => setUser(e.target.value)}
+                      value={user}
+                      required
+                  />
+
+                  <label htmlFor="password">Password:</label>
+                  <input
+                      type="password"
+                      id="password"
+                      onChange={(e) => setPwd(e.target.value)}
+                      value={pwd}
+                      required
+                  />
+                  <button>Sign In</button>
+              </form>
+              <p>
+                  Need an Account?<br />
+                  <span className="line">
+                      {/*put router link here*/}
+                      <a href="#">Sign Up</a>
+                  </span>
+              </p>
+          </section>
+        )}    
+    </>  
   )
 }
 
