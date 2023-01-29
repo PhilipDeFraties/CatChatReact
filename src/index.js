200fc52628ff2ebf9e516c246a37cdc655e8299b
PhilipDeFraties <philipjfry338@gmail.com>
2023-01-28 20:01:19 -0700

    edits index.js to wrap rendered App in BrowserRouter, AuthProvider, and Routes to return single route containing App as element

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 )
